5678c28932ad06a0481f9cb6df7e45585010f1ad
Kevin Filipe <kevinfillipe@gmail.com>
2014-12-11 22:10:01 -0300

    Adiciona complemento do fornecedor

---
+++ app/models/objects/FornecedorComplemento.java
@@ -0,0 +1,46 @@
+package models.objects;
+
+import models.Fornecedor;
+import play.db.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class FornecedorComplemento extends Model {
+
+    @OneToOne
+    private Fornecedor fornecedor;
+
+    @Column(name = "INSCESTADUAL")
+    private String inscricaoEstadual;
+
+    @Column(name = "INSCMUNICIPAL")
+    private String inscricaoMunicipal;
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getInscricaoMunicipal() {
+        return inscricaoMunicipal;
+    }
+
+    public void setInscricaoMunicipal(String inscricaoMunicipal) {
+        this.inscricaoMunicipal = inscricaoMunicipal;
+    }
+}
